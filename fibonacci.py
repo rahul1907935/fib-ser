fab8d9dfc00cce92a9b4d3d17bcbe708f74a08d9
rahul1907935 <83805686+rahul1907935@users.noreply.github.com>
2021-08-29 12:43:03 +0530

    Python Program on Fibonacci Series
    This file has been printed successfully

---
+++ fibonacci.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 27 02:03:58 2021
+
+@author: rahul
+"""
+
+nterms = int(input("How many terms? "))
+
+n1, n2 = 0, 1
+count = 0
+
+
+if nterms <= 0:
+   print("Please enter a positive integer")
+   
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       
+       n1 = n2
+       n2 = nth
+       count += 1
